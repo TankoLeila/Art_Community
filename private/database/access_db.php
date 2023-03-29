73d0f93affcca4a11c029561e583bb293d969c73
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-03-29 03:38:35 +0100

    résolution des erreurs

--- private/database/access_db.php
+++ private/database/access_db.php
@@ -1,9 +1,9 @@
 <?php
-    $pdo = new PDO('mysql:dbname=art_community_db;host=localhost', 'root', '')
+    $pdo = new PDO('mysql:dbname=art_community_db;host=localhost', 'root', '');
 
-    $pdo -> setAttribute(PDO::ATTR_ERRORMODE, PDO::ERRMODE_EXCEPTION);
+    //$pdo -> setAttribute(PDO::ATTR_ERRORMODE, PDO::ERRMODE_EXCEPTION);
 
     if(!$pdo){
-        die("connection failed")
+        die("connection failed");
     }
 ?>
